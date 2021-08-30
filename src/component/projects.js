148548e96f8a4bb5eb43b672e68027579fbc0745
Ayushak18 <aayushkamboj7@gmail.com>
2021-08-30 17:58:49 +0530

    Project Page structure

--- src/component/projects.js
+++ src/component/projects.js
@@ -1,5 +1,79 @@
+import '../styles/project.css';
+
 function Projects() {
-  return <h1>From Projects</h1>;
+  return (
+    <section className="projects">
+      <h1 className="project-section-heading">Projects</h1>
+      <div className="project">
+        <div className="project-img">
+          <img src="laptop.jpg" alt="Project" />
+        </div>
+        <div className="project-description">
+          <h1>Project Name</h1>
+          <p>Short Descrition</p>
+          <a href="" className="source-code">
+            <i class="fab fa-github"></i>
+            Source Code
+          </a>
+          <a href="" className="demo-link">
+            <i class="fas fa-play"></i>
+            Demo Link
+          </a>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project-img">
+          <img src="laptop.jpg" alt="Project" />
+        </div>
+        <div className="project-description">
+          <h1>Project Name</h1>
+          <p>Short Descrition</p>
+          <a href="" className="source-code">
+            <i class="fab fa-github"></i>
+            Source Code
+          </a>
+          <a href="" className="demo-link">
+            <i class="fas fa-play"></i>
+            Demo Link
+          </a>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project-img">
+          <img src="laptop.jpg" alt="Project" />
+        </div>
+        <div className="project-description">
+          <h1>Project Name</h1>
+          <p>Short Descrition</p>
+          <a href="" className="source-code">
+            <i class="fab fa-github"></i>
+            Source Code
+          </a>
+          <a href="" className="demo-link">
+            <i class="fas fa-play"></i>
+            Demo Link
+          </a>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project-img">
+          <img src="laptop.jpg" alt="Project" />
+        </div>
+        <div className="project-description">
+          <h1>Project Name</h1>
+          <p>Short Descrition</p>
+          <a href="" className="source-code">
+            <i class="fab fa-github"></i>
+            Source Code
+          </a>
+          <a href="" className="demo-link">
+            <i class="fas fa-play"></i>
+            Demo Link
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
