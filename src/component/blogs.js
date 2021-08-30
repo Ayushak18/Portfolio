f1778a830ed7537c46e6790e47dba62893991b75
Ayushak18 <aayushkamboj7@gmail.com>
2021-08-30 18:12:07 +0530

    Blog Basic Structure

--- src/component/blogs.js
+++ src/component/blogs.js
@@ -1,5 +1,24 @@
+import '../styles/blog.css';
+
 function Blogs() {
-  return <h1>From Blogs Component</h1>;
+  return (
+    <section className="blogs">
+      <h1 className="blog-section-heading">Blogs</h1>
+      <div className="blog">
+        <div className="blog-img">
+          <img src="laptop.jpg" alt="Project" />
+        </div>
+        <div className="blog-description">
+          <h1>Blog Name</h1>
+          <p>Short Descrition</p>
+          <a href="" className="demo-link">
+            <i class="fas fa-book"></i>
+            Blog
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Blogs;
