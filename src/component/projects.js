d0c1d57aaa1fc247b85a53ffbf8c00b5d3129099
Ayushak18 <aayushkamboj7@gmail.com>
2021-09-12 14:34:02 +0530

    Projects updated

--- src/component/projects.js
+++ src/component/projects.js
@@ -6,6 +6,36 @@ function Projects() {
       <h1 className="project-section-heading">Projects</h1>
       <div className="project">
         <div className="project-img">
+          <img src="Portfolio.png" alt="Project" />
+        </div>
+        <div className="project-description">
+          <h1>Portfolio</h1>
+          <p>
+            Created a static clone of a cleaning company which contains various
+            pages using HTML, CSS.
+          </p>
+          <a
+            href="https://github.com/Ayushak18/Portfolio"
+            className="source-code"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-github"></i>
+            Source Code
+          </a>
+          <a
+            href="https://www.ayushkamboj.tech"
+            className="demo-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fas fa-play"></i>
+            Demo Link
+          </a>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project-img">
           <img src="blog.png" alt="Project" />
         </div>
         <div className="project-description">
@@ -16,7 +46,7 @@ function Projects() {
             JavaScript, HTML, CSS.
           </p>
           <a
-            href="https://github.com/Ayushak18/Blog-Application "
+            href="https://github.com/Ayushak18/Blog-Application"
             className="source-code"
             target="_blank"
             rel="noreferrer"
@@ -25,7 +55,7 @@ function Projects() {
             Source Code
           </a>
           <a
-            href="https://brave-panini-e9d918.netlify.app/"
+            href="https://fervent-mestorf-9b4307.netlify.app/"
             className="demo-link"
             target="_blank"
             rel="noreferrer"
@@ -56,7 +86,7 @@ function Projects() {
             Source Code
           </a>
           <a
-            href="https://trusting-visvesvaraya-6cc26d.netlify.app/"
+            href="https://github-battle-three.vercel.app/battle"
             className="demo-link"
             target="_blank"
             rel="noreferrer"
@@ -66,6 +96,39 @@ function Projects() {
           </a>
         </div>
       </div>
+      {/* <div className="project">
+        <div className="project-img">
+          <img
+            src="https://images.unsplash.com/photo-1585432959315-d9342fd58eb6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+            alt="Project"
+          />
+        </div>
+        <div className="project-description">
+          <h1>Quiz App</h1>
+          <p>
+            Created a static clone of a cleaning company which contains various
+            pages using HTML, CSS.
+          </p>
+          <a
+            href="https://github.com/Ayushak18/Quiz-App"
+            className="source-code"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-github"></i>
+            Source Code
+          </a>
+          <a
+            href="https://quiz-app-eta-khaki.vercel.app/"
+            className="demo-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fas fa-play"></i>
+            Demo Link
+          </a>
+        </div>
+      </div> */}
       <div className="project">
         <div className="project-img">
           <img src="todo.png" alt="Project" />
@@ -96,6 +159,127 @@ function Projects() {
           </a>
         </div>
       </div>
+      <div className="project">
+        <div className="project-img">
+          <img src="clock.png" alt="Project" />
+        </div>
+        <div className="project-description">
+          <h1>JavaScript Clock</h1>
+          <p>
+            Created a static clone of a cleaning company which contains various
+            pages using HTML, CSS.
+          </p>
+          <a
+            href="https://github.com/Ayushak18/JavaScriptClock"
+            className="source-code"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-github"></i>
+            Source Code
+          </a>
+          <a
+            href="https://elated-curie-04ba03.netlify.app/"
+            className="demo-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fas fa-play"></i>
+            Demo Link
+          </a>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project-img">
+          <img src="responsive.png" alt="Project" />
+        </div>
+        <div className="project-description">
+          <h1>Responsive Web Page</h1>
+          <p>
+            Created a static clone of a cleaning company which contains various
+            pages using HTML, CSS.
+          </p>
+          <a
+            href="https://github.com/Ayushak18/ResponsivePageClone"
+            className="source-code"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-github"></i>
+            Source Code
+          </a>
+          <a
+            href="https://wizardly-hopper-a870e1.netlify.app/"
+            className="demo-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fas fa-play"></i>
+            Demo Link
+          </a>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project-img">
+          <img src="bookly.png" alt="Project" />
+        </div>
+        <div className="project-description">
+          <h1>Bookly</h1>
+          <p>
+            Created a static clone of a cleaning company which contains various
+            pages using HTML, CSS.
+          </p>
+          <a
+            href="https://github.com/Ayushak18/Bookly"
+            className="source-code"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-github"></i>
+            Source Code
+          </a>
+          <a
+            href="https://epic-mcclintock-3cf429.netlify.app/"
+            className="demo-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fas fa-play"></i>
+            Demo Link
+          </a>
+        </div>
+      </div>
+      <div className="project">
+        <div className="project-img">
+          <img src="got.png" alt="Project" />
+        </div>
+        <div className="project-description">
+          <h1>Game Of Thrones</h1>
+          <p>
+            Created a static clone of a cleaning company which contains various
+            pages using HTML, CSS.
+          </p>
+          <a
+            href="https://github.com/Ayushak18/GOT"
+            className="source-code"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-github"></i>
+            Source Code
+          </a>
+          <a
+            href="https://boring-bassi-32d8e5.netlify.app/"
+            className="demo-link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fas fa-play"></i>
+            Demo Link
+          </a>
+        </div>
+      </div>
+
       <div className="project">
         <div className="project-img">
           <img src="buson.png" alt="Project" />
